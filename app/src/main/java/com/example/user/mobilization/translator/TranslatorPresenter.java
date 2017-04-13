76c18c06003124a074b59e3161021f18ce10aeab
User <User@MacBook-Pro-39appleru.local>
2017-04-13 00:25:02 -0700

    Start TranslatorFrafment

--- app/src/main/java/com/example/user/mobilization/translator/TranslatorPresenter.java
+++ app/src/main/java/com/example/user/mobilization/translator/TranslatorPresenter.java
@@ -1,8 +1,10 @@
 package com.example.user.mobilization.translator;
 
+import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
+
 /**
  * Created by User on 12.04.17.
  */
 
-public class TranslatorPresenter {
+public class TranslatorPresenter extends MvpBasePresenter<TranslatorView> {
 }
--- app/src/main/java/com/example/user/mobilization/translator/TranslatorView.java
+++ app/src/main/java/com/example/user/mobilization/translator/TranslatorView.java
@@ -1,8 +1,10 @@
 package com.example.user.mobilization.translator;
 
+import com.hannesdorfmann.mosby3.mvp.MvpView;
+
 /**
  * Created by User on 12.04.17.
  */
 
-public class TranslatorView {
+interface TranslatorView extends MvpView {
 }
