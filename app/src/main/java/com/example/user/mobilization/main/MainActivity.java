0d558be89a49df60b7e7eee034278902293998a1
User <User@MacBook-Pro-39appleru.local>
2017-04-12 22:59:12 -0700

    Add architecture

--- app/src/main/java/com/example/user/mobilization/main/MainActivity.java
+++ app/src/main/java/com/example/user/mobilization/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.user.mobilization;
+package com.example.user.mobilization.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.user.mobilization.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/user/mobilization/translator/TranslatorView.java
@@ -0,0 +1,8 @@
+package com.example.user.mobilization.translator;
+
+/**
+ * Created by User on 12.04.17.
+ */
+
+public class TranslatorView {
+}
