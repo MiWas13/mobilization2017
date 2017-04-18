ac2503ee298626769628e061d07588ae5c8d2ac4
User <User@MacBook-Pro-39appleru.local>
2017-04-17 22:07:47 -0700

    Refactor classes

---
+++ app/src/main/java/com/example/user/mobilization/ui/bookmarks/BookmarksPresenter.java
@@ -0,0 +1,8 @@
+package com.example.user.mobilization.ui.bookmarks;
+
+/**
+ * Created by User on 12.04.17.
+ */
+
+public class BookmarksPresenter {
+}
--- app/src/main/java/com/example/user/mobilization/ui/history/HistoryFragment.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.mobilization.ui.history;
-
-/**
- * Created by User on 12.04.17.
- */
-
-public class HistoryFragment {
-}
