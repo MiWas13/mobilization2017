f756880e5e2c35b61a6f031bf99698d717be3613
User <User@MacBook-Pro-39appleru.local>
2017-04-18 03:01:38 -0700

    WIP recycler in bookmarks

--- app/src/main/java/com/example/user/mobilization/ui/base/BaseBookmarksFragment.java
+++ app/src/main/java/com/example/user/mobilization/ui/base/BaseBookmarksFragment.java
@@ -2,11 +2,13 @@ package com.example.user.mobilization.ui.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.user.mobilization.R;
+import com.example.user.mobilization.ui.bookmarks.BookmarksAdapter;
 import com.example.user.mobilization.ui.bookmarks.BookmarksView;
 import com.hannesdorfmann.mosby3.mvp.MvpFragment;
 
@@ -19,11 +21,6 @@ public class BaseBookmarksFragment extends MvpFragment<BookmarksView, BaseBookma
     private View view;
 
     @Override
-    public void initView() {
-
-    }
-
-    @Override
     public BaseBookmarksPresenter createPresenter() {
         return new BaseBookmarksPresenter();
     }
@@ -33,5 +30,12 @@ public class BaseBookmarksFragment extends MvpFragment<BookmarksView, BaseBookma
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.bookmarks_fragment, null);
         return view;
+    }
+
+    @Override
+    public void initView() {
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        BookmarksAdapter bookmarksAdapter = new BookmarksAdapter();
+        recyclerView.setAdapter(bookmarksAdapter);
     }
 }
--- app/src/main/java/com/example/user/mobilization/ui/bookmarks/BookmarksAdapter.java
+++ app/src/main/java/com/example/user/mobilization/ui/bookmarks/BookmarksAdapter.java
@@ -1,8 +1,53 @@
 package com.example.user.mobilization.ui.bookmarks;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.mobilization.R;
+import com.example.user.mobilization.model.BookmarkModel;
+
+import java.util.ArrayList;
+
+
 /**
  * Created by User on 17.04.17.
  */
 
-public class BookmarksAdapter {
+public class BookmarksAdapter extends RecyclerView.Adapter<BookmarksAdapter.ViewHolder> {
+    private ArrayList<BookmarkModel> data = new ArrayList<>();
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bookmarks, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public void setData(ArrayList<BookmarkModel> data) {
+        this.data = data;
+    }
+
 }
