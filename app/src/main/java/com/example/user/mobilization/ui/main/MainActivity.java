7613686facfd5fbe8557a405abb33e1ff039258d
User <User@MacBook-Pro-39appleru.local>
2017-04-23 18:14:20 -0700

    Change tabs names

--- app/src/main/java/com/example/user/mobilization/ui/main/MainActivity.java
+++ app/src/main/java/com/example/user/mobilization/ui/main/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.user.mobilization.ui.main;
 
 
-import android.content.Context;
-import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.view.inputmethod.InputMethodManager;
 
 import com.example.user.mobilization.R;
 import com.example.user.mobilization.ui.bookmarks.TabsFragment;
 import com.example.user.mobilization.ui.translator.TranslatorFragment;
 import com.hannesdorfmann.mosby3.mvp.MvpActivity;
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends MvpActivity<MainView, MainPresenter> implements MainView {
 
@@ -43,7 +39,7 @@ public class MainActivity extends MvpActivity<MainView, MainPresenter> implement
                     presenter.setFragment(new TabsFragment());
                     break;
                 case R.id.settings_button:
-//                      presenter.setFragment();
+//                    presenter.setFragment(new TranslatorFragment());
                     break;
             }
         });
