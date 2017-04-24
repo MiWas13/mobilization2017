3e7164bcb8a6f3b1e19cc5c95a488e874434d21b
User <User@MacBook-Pro-39appleru.local>
2017-04-24 04:05:04 -0700

    Fix bookmarks

--- app/src/main/java/com/example/user/mobilization/ui/base/BaseBookmarksFragment.java
+++ app/src/main/java/com/example/user/mobilization/ui/base/BaseBookmarksFragment.java
@@ -1,12 +1,10 @@
 package com.example.user.mobilization.ui.base;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
